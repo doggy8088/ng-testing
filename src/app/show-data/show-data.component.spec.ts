3b5d9e1e3ef6d2445d0324f3b2aadc05036c47ef
Will <doggy.huang@gmail.com>
2018-06-10 15:26:27 +0800

    改用 jasmine.createSpyObj 來模擬 HttpClient 服務物件

--- src/app/show-data/show-data.component.spec.ts
+++ src/app/show-data/show-data.component.spec.ts
@@ -1,52 +1,110 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { ShowDataComponent } from './show-data.component';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 describe('ShowDataComponent', () => {
   let component: ShowDataComponent;
   let fixture: ComponentFixture<ShowDataComponent>;
   let httpController: HttpTestingController;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ShowDataComponent ],
-      imports: [ HttpClientTestingModule ]
-    })
-    .compileComponents();
-  }));
+  describe('Testing by HttpClientTestingModule', () => {
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ShowDataComponent);
-    component = fixture.componentInstance;
-    httpController = TestBed.get(HttpTestingController);
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ ShowDataComponent ],
+        imports: [ HttpClientTestingModule ]
+      })
+      .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  // HttpTestingController
-  // https://angular.io/api/common/http/testing/HttpTestingController
-  it('should call api to URL', () => {
-    const mockResult = { name: 'Kevin' };
-    const expectedValue = 'Kevin!!';
-    component.getData().subscribe(value => {
-      expect(value).toEqual(expectedValue);
+    beforeEach(() => {
+      fixture = TestBed.createComponent(ShowDataComponent);
+      component = fixture.componentInstance;
+      httpController = TestBed.get(HttpTestingController);
+      fixture.detectChanges();
     });
 
-    // Method 1
-    // const req = httpController.expectOne('myurl');
-    // expect(req.request.method).toEqual('GET');
-
-    // Method 2
-    const req = httpController.expectOne({
-      method: 'GET',
-      url: 'myurl'
+    it('should create', () => {
+      expect(component).toBeTruthy();
     });
 
-    req.flush(mockResult); // 這一行才會讓 HTTP 正式發出
+    // HttpTestingController
+    // https://angular.io/api/common/http/testing/HttpTestingController
+    it('should call api to URL', () => {
+      const mockResult = { name: 'Kevin' };
+      const expectedValue = 'Kevin!!';
+      component.getData().subscribe(value => {
+        expect(value).toEqual(expectedValue);
+      });
 
-    httpController.verify();
+      // Method 1
+      // const req = httpController.expectOne('myurl');
+      // expect(req.request.method).toEqual('GET');
+
+      // Method 2
+      const req = httpController.expectOne({
+        method: 'GET',
+        url: 'myurl'
+      });
+
+      req.flush(mockResult); // 這一行才會讓 HTTP 正式發出
+
+      httpController.verify();
+    });
+
+    it('use spyOn', () => {
+      const mockResult = { name: 'Kevin' };
+      const expectedValue = 'Kevin!!';
+
+      spyOn(component['http'], 'get').and.returnValue(of(mockResult));
+
+      component.getData().subscribe(value => {
+        expect(component['http'].get).toHaveBeenCalledWith('myurl');
+        expect(value).toEqual(expectedValue);
+      })
+    });
+
   });
+
+
+  describe('Testing by jasmine.createSpyObj', () => {
+    let spyHttpGet = jasmine.createSpyObj('http', ['get']);
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ ShowDataComponent ],
+        providers: [
+          {
+            provide: HttpClient,
+            useValue: spyHttpGet
+          }
+        ]
+      })
+      .compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(ShowDataComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('use spyObject', () => {
+
+      const mockResult = { name: 'Kevin' };
+      const expectedValue = 'Kevin!!';
+
+      spyHttpGet.get.and.returnValue(of(mockResult));
+
+      component.getData().subscribe(value => {
+        expect(component['http'].get).toHaveBeenCalledWith('myurl');
+        expect(value).toEqual(expectedValue);
+      });
+
+    });
+
+  });
+
 });
