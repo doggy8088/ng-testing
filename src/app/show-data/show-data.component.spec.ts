e910e2c1c1647f549f4e436231c3b1377b84c870
Will <doggy.huang@gmail.com>
2018-06-10 14:59:29 +0800

    透過 HttpClientTestingModule 測試 HttpClient 的呼叫與執行

--- src/app/show-data/show-data.component.spec.ts
+++ src/app/show-data/show-data.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { ShowDataComponent } from './show-data.component';
 
 describe('ShowDataComponent', () => {
   let component: ShowDataComponent;
   let fixture: ComponentFixture<ShowDataComponent>;
+  let httpController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShowDataComponent ]
+      declarations: [ ShowDataComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   }));
@@ -16,10 +18,34 @@ describe('ShowDataComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ShowDataComponent);
     component = fixture.componentInstance;
+    httpController = TestBed.get(HttpTestingController);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // HttpTestingController
+  // https://angular.io/api/common/http/testing/HttpTestingController
+  it('should call api to URL', () => {
+    const mockResult = { name: 'Kevin' };
+    component.getData().subscribe(value => {
+      expect(value).toEqual(mockResult);
+    });
+
+    // Method 1
+    // const req = httpController.expectOne('myurl');
+    // expect(req.request.method).toEqual('GET');
+
+    // Method 2
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: 'myurl'
+    });
+
+    req.flush(mockResult); // 這一行才會讓 HTTP 正式發出
+
+    httpController.verify();
   });
 });
--- src/app/show-data/show-data.component.ts
+++ src/app/show-data/show-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-show-data',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowDataComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  getData() {
+    return this.http.get('myurl');
+  }
 }
